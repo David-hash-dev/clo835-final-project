814323a3a70afc8e2e29e77f3587e89f77d8d367
EC2 Default User <ec2-user@ip-172-31-91-17.ec2.internal>
2025-07-31 23:41:47 +0000

    Update app.py and index.html to support dynamic backgroung from configmap

--- app.py
+++ app.py
@@ -1,31 +1,22 @@
 import os
-import boto3
+import time
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-s3_image_url = os.environ.get("S3_IMAGE_URL")
-group_name = os.environ.get("GROUP_NAME", "Your Group Name")
-group_slogan = os.environ.get("GROUP_SLOGAN", "Your Slogan")
-image_file = "static/background.jpg"
-
-print(f"Background Image URL: {s3_image_url}")
-
-def download_image_from_s3(url):
-    # Ensure folder exists
-    os.makedirs(os.path.dirname(image_file), exist_ok=True)
-    
-    s3 = boto3.client('s3', region_name='us-east-1')
-    bucket_name = url.split('/')[2].split('.')[0]
-    key = '/'.join(url.split('/')[3:])
-    s3.download_file(bucket_name, key, image_file)
-
-if s3_image_url:
-    download_image_from_s3(s3_image_url)
-
 @app.route("/")
 def index():
-    return render_template("index.html", group_name=group_name, group_slogan=group_slogan)
+    background_image_url = os.environ.get("BACKGROUND_IMAGE_URL", "")
+    group_name = os.environ.get("GROUP_NAME", "Your Group Name")
+    group_slogan = os.environ.get("GROUP_SLOGAN", "Your Slogan")
+    cache_buster = int(time.time())  # helps bust browser cache
+    return render_template(
+        "index.html",
+        group_name=group_name,
+        group_slogan=group_slogan,
+        background_image_url=background_image_url,
+        cache_buster=cache_buster
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8081, debug=True)
