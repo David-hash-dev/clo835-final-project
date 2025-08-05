446ea325ccbf53f2ac9599c0fc33d84b70639946
EC2 Default User <ec2-user@ip-172-31-33-213.ec2.internal>
2025-08-05 20:52:39 +0000

    Update app.py details

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def index():
     background_image_url = os.environ.get("BACKGROUND_IMAGE_URL", "")
     group_name = os.environ.get("GROUP_NAME", "Group 18")
-    group_slogan = os.environ.get("GROUP_SLOGAN", "Together, we got this!")
+    group_slogan = os.environ.get("GROUP_SLOGAN", "Together, we got this!!!")
     cache_buster = int(time.time())  # helps bust browser cache
     return render_template(
         "index.html",
